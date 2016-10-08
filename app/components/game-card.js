35ba1b820ad70de28d90c9f735f83293b92e3331
Giancarlo <guerragiancarlo8@gmail.com>
2016-10-09 00:24:23 +0200

    added flip effect for cards

--- app/components/game-card.js
+++ app/components/game-card.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ['flip-container'],
+  actions: {
+    addToArray() {
+      console.log('i was clicked!');
+    }
+  }
 });
---
+++ app/helpers/shuffle-array.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export function shuffleArray(array) {
+
+  var m = array.length, t, i;
+
+  while (m) {
+
+    i = Math.floor(Math.random() * m--);
+
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+};
+
+
+export default Ember.Helper.helper(shuffleArray);
