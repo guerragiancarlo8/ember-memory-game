c54b81e0a942ca42a2b6459e7099124c1c9db44f
Giancarlo <guerragiancarlo8@gmail.com>
2016-10-09 16:03:30 +0200

    implemented very basic turn validation logic

--- app/components/game-card.js
+++ app/components/game-card.js
@@ -2,7 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['flip-container'],
-  click() {
-    this.sendAction('action', this.symbol);
-  }
+  isFlipped: false
 });
--- app/controllers/games/play.js
+++ app/controllers/games/play.js
@@ -1,10 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  clickedCard: null,
+  init() {
+    this.set('clickedCard', '');
+  },
 
   actions: {
-    proveImAlive(someUrl) {
-      console.log('Im aliiiiveeee' + someUrl);
+    handleCardClick(someUrl) {
+      if(this.clickedCard === '') {
+        //we store first clicked card
+        this.set('clickedCard', someUrl);
+      } else if(someUrl === this.clickedCard){
+        //we assume the card clicked was a match. winning pair. we reset the variable;
+        console.log('we have a matching pair');
+        //logic to maintain the two cards with the image flipped
+        this.set('clickedCard', '');
+      } else if(this.clickedCard !== '' && someUrl !== this.clickedCard){
+        console.log('nope. restart');
+        this.set('clickedCard', '');
+        //reset the clickedCard variable;
+        //we assume the second clicked card wasn't a match. so we reset
+      }
     }
   }
 });
